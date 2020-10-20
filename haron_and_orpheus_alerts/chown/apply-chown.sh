7969c987a7d603325dca16b7a2cc2542d22cac07
Sadovin Viktor <v.sadovin@corp.mail.ru>
2020-10-20 17:03:29 +0300

    fix

--- haron_and_orpheus_alerts/chown/apply-chown.sh
+++ haron_and_orpheus_alerts/chown/apply-chown.sh
@@ -4,10 +4,11 @@ set -x
 HOSTNAME=`hostname | awk 'BEGIN{FS="."}{print $1}'`
 PATH_TO_FILE=/home/v.sadovin/${HOSTNAME}.files.txt
 
-rm $PATH_TO_FILE
-#readarray -t files < ${PATH_TO_FILE}
 
-#for file in "${files[@]}"
-#do
-        #chown nobody:nobody $file
-#done
+readarray -t files < ${PATH_TO_FILE}
+
+for file in "${files[@]}"
+do
+        chown nobody:nobody $file
+done
+rm $PATH_TO_FILE
