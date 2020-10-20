20881f08c44d611d97f79ee7605a91b70ed54952
Sadovin Viktor <v.sadovin@corp.mail.ru>
2020-10-20 16:46:10 +0300

    Modified hosts

--- haron_and_orpheus_alerts/files/deliver-files.sh
+++ haron_and_orpheus_alerts/files/deliver-files.sh
@@ -11,13 +11,3 @@ do
         cat $PATH_TO_LOGS | grep $host | awk 'BEGIN{FS=" "}{print $10}' | uniq > $host.files.txt
         rsync -azh $host.files.txt $host:/home/v.sadovin
 done
-
-# haron_count="`ps aux | grep haron | wc -l`"
-
-# if [ "$haron_count" == "2" ]; then
-#     echo "Can chown"
-# else
-#     echo "Can't chown"
-# fi
-
-# ionice -c 2 -n 3 chown -R nobody:nobody /storage/*
